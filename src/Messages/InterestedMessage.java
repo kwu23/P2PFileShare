aa009c51ae39e1be825fbfaa5054c071f1d83e27
kwu23 <kev.k.wu@gmail.com>
2016-10-19 17:42:50 -0400

    implemented interested and not interested messages

--- src/Messages/InterestedMessage.java
+++ src/Messages/InterestedMessage.java
@@ -3,7 +3,28 @@ package Messages;
 /**
  * Created by kevinwu on 10/19/16.
  */
-public class InterestedMessage {
+public class InterestedMessage extends Message {
     private int value = 2;
+    private int length = 1;
 
+
+    @Override
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String getPayload() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return null;
+    }
 }
--- src/Messages/NotInterestedMessage.java
+++ src/Messages/NotInterestedMessage.java
@@ -3,6 +3,27 @@ package Messages;
 /**
  * Created by kevinwu on 10/19/16.
  */
-public class NotInterestedMessage {
+public class NotInterestedMessage extends Message{
     private int value = 3;
+    private int length = 1;
+
+    @Override
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String getPayload() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return null;
+    }
 }
