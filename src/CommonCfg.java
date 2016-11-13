8141cef0e38b0435802335b0e4d6cbc728161dc7
kwu23 <kev.k.wu@gmail.com>
2016-11-13 15:41:11 -0500

    test

--- src/CommonCfg.java
+++ src/CommonCfg.java
@@ -7,12 +7,12 @@ import java.util.List;
  * Created by kevinwu on 10/19/16.
  */
 public class CommonCfg {
-    private static int numberOfPreferredNeighbors;
-    private static int unchokingInterval;
-    private static int optimisticUnchokingInterval;
-    private static String fileName;
-    private static int fileSize;
-    private static int pieceSize;
+    public static int numberOfPreferredNeighbors;
+    public static int unchokingInterval;
+    public static int optimisticUnchokingInterval;
+    public static String fileName;
+    public static int fileSize;
+    public static int pieceSize;
 
     public CommonCfg(String fileName) {
         List<String> lines;
