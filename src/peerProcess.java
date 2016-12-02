3343b3ff62c244d39886c3a8a2abc29f08df042f
kwu23 <kev.k.wu@gmail.com>
2016-12-02 15:42:56 -0500

    test prints

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -310,27 +310,20 @@ public class peerProcess {
                     numOfInterestedPeers++;
                 }
             }
-
-            if(k > numOfInterestedPeers){
+            if(preferredNeighbors.contains(neighbor.getPeerID())){
+                preferredNeighbors.remove(preferredNeighbors.indexOf(neighbor.getPeerID()));
+            }
+            if(Math.random() < (double) k/ (double) numOfInterestedPeers && interested && preferredNeighbors.size() < k) {
+                preferredNeighbors.add(neighbor.getPeerID());
                 if(!preferredNeighbors.contains(neighbor.getPeerID())){
                     sendMessage(out, new UnchokeMessage());
-                    preferredNeighbors.add(neighbor.getPeerID());
                     System.out.println("New rand unchoke msg sent to " + neighbor.getPeerID());
                 }
-            }
-            else {
-                if (Math.random() < (double) k / (double) numOfInterestedPeers && interested && preferredNeighbors.size() < k) {
-                    if (!preferredNeighbors.contains(neighbor.getPeerID())) {
-                        sendMessage(out, new UnchokeMessage());
-                        preferredNeighbors.add(neighbor.getPeerID());
-                        System.out.println("New rand unchoke msg sent to " + neighbor.getPeerID());
-                    }
-                } else {
-                    if (preferredNeighbors.contains(neighbor.getPeerID())) {
-                        System.out.println("New rand choke msg sent to " + neighbor.getPeerID());
-                        preferredNeighbors.remove(preferredNeighbors.indexOf(neighbor.getPeerID()));
-                        sendMessage(out, new ChokeMessage());
-                    }
+            }else{
+                if(preferredNeighbors.contains(neighbor.getPeerID())){
+                    System.out.println("New rand choke msg sent to " + neighbor.getPeerID());
+                    preferredNeighbors.remove(preferredNeighbors.indexOf(neighbor.getPeerID()));
+                    sendMessage(out, new ChokeMessage());
                 }
             }
         }
